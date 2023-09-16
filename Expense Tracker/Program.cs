a2def93ef10f9786869d8f61fc9a5cc0bf848c44
Ben Hur <bhur777@gmail.com>
2023-09-15 21:29:34 -0400

    Create DbContenxt

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -1,7 +1,13 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DI
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer());
 
 var app = builder.Build();
 
