503e2f3f8b72ca878063cf8c0e9f22bf911da918
Ben Hur <bhur777@gmail.com>
2023-09-17 20:20:27 -0400

    add template for Type display and iconon categories

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -9,5 +9,12 @@ namespace Expense_Tracker.Models
         [Column(TypeName = "nvarchar(50)")] public string Title { get; set; }
         [Column(TypeName = "nvarchar(5)")] public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")] public string Type { get; set; } = "Expense";
+
+        [NotMapped] public string? TitleWithIcon {  
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
