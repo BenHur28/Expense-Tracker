06c31639c46af94ad24d7859eaac8dd1918115a3
Ben Hur <bhur777@gmail.com>
2023-09-18 23:05:42 -0400

    add validation for transaction and category

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -6,7 +6,9 @@ namespace Expense_Tracker.Models
     public class Category
     {
         [Key] public int CategoryId { get; set; }
-        [Column(TypeName = "nvarchar(50)")] public string Title { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = " Title is required")]
+        public string Title { get; set; }
         [Column(TypeName = "nvarchar(5)")] public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")] public string Type { get; set; } = "Expense";
 
